5071aa50a0ef909d044cca2a820895b274f75baa
Nenti Nurnaningsih <81434333+nentinur@users.noreply.github.com>
2022-03-16 22:21:14 +0700

    Add files via upload

---
+++ src/main/java/id/ac/uin/student/entity/Student.java
@@ -0,0 +1,32 @@
+package id.ac.uin.student.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "student")
+@Data
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotEmpty
+    @Column(nullable = false, name = "first_name")
+    private String firstName;
+
+    @NotEmpty
+    @Column(nullable = false, name = "last_name")
+    private String lastName;
+
+    @NotEmpty
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @NotEmpty
+    @Column(name = "phone_number", unique = true)
+    private String phoneNumber;
+
+}
